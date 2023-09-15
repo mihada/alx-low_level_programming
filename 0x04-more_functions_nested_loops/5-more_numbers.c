690e91e5a17936833f07787e3bce176308970f0f
mihada <mihad1996@gmail.com>
2023-09-15 03:07:33 +0300

    functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,22 @@ include "main.h"
 
 /**
  * more_numbers - print more numbers
+ * Return: void
  */
 
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar (j % 10 + '0');
+				_putchar(j + '0');
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
+		_putchar('\n');
 }
 
